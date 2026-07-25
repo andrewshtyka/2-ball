7d9f6459670cd7819ae5b07cd416c8a307a07f7e
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-07-25 11:01:54 +0300

    init project

---
+++ src/3d-scene/cameraConfig.js
@@ -0,0 +1,8 @@
+import { canvasSizes } from "./canvasSizes";
+
+export const cameraConfig = {
+  fov: 40,
+  aspect: canvasSizes.aspect,
+  near: 0.1,
+  far: 100,
+};
---
+++ src/3d-scene/canvasSizes.js
@@ -0,0 +1,5 @@
+export const canvasSizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  aspect: window.innerWidth / window.innerHeight,
+};
